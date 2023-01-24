09120ebed2670c7fdd57c47990e3e6146e971b38
Nicolas Quinquenel <nicolas.quinquenel@sonarsource.com>
2023-01-24 10:23:28 +0100

    Annotator customizations

--- src/main/kotlin/org/sonar/fakeplugin/language/SimpleAnnotator.kt
+++ src/main/kotlin/org/sonar/fakeplugin/language/SimpleAnnotator.kt
@@ -1,10 +1,12 @@
 package org.sonar.fakeplugin.language
 
+import com.intellij.codeInspection.ProblemHighlightType
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.psi.PsiElement
+import com.intellij.psi.util.elementType
 
 class SimpleAnnotator : Annotator {
 
@@ -14,18 +16,26 @@ class SimpleAnnotator : Annotator {
 
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
         val value = element.text ?: return
-
-        if (value != WORD_ANNOTATED) {
-            return
-        }
-
         val textRange = element.textRange
+        val elementType = element.elementType.toString()
 
-
-        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
-            .range(textRange)
-            .textAttributes(DefaultLanguageHighlighterColors.KEYWORD)
-            .create()
+        if (elementType == "PACKAGE_DIRECTIVE") {
+            holder.newAnnotation(HighlightSeverity.WARNING, "This is a package")
+                .range(textRange)
+                .highlightType(ProblemHighlightType.WEAK_WARNING)
+                .create()
+        } else if (elementType == "IMPORT_DIRECTIVE") {
+            holder.newAnnotation(HighlightSeverity.INFORMATION, "This is an import")
+                .range(textRange)
+                .highlightType(ProblemHighlightType.GENERIC_ERROR)
+                .tooltip("This is a tooltip")
+                .create()
+        } else if (value == WORD_ANNOTATED) {
+            holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                .range(textRange)
+                .textAttributes(DefaultLanguageHighlighterColors.STATIC_FIELD)
+                .create()
+        }
     }
 
 }
